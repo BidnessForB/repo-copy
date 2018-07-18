fec2a18efcb8e2593f9c016d804f7f786358516b
Bryan Cross <bryancross@github.com>
2018-07-18 08:16:53 -0500

    Fixing linter/ignore issues

--- createRepoController.js
+++ createRepoController.js
@@ -5,70 +5,72 @@ const http = require('http');
 try {
     initHTTPServer();
 }
-catch(err)
-{
+catch (err) {
     console.log("Error starting server: " + err.message);
     process.exit(1);
 }
 
 function initHTTPServer() {
     var dispatcher = new HttpDispatcher();
-    dispatcher.onPost('/createRepo', function(req,res)
-    {
+    dispatcher.onPost('/createRepo', function (req, res) {
         var repo = new Repo();
-        repo.executeRest(req,res,'create');
+        repo.executeRest(req, res, 'create');
     });
-    dispatcher.onPost('/getRepoConfig', function(req,res) {
+    dispatcher.onPost('/getRepoConfig', function (req, res) {
         var repo = new Repo();
-        repo.executeRest(req,res,'get');
+        repo.executeRest(req, res, 'get');
     });
-    dispatcher.onPost('/repoAudit',function(req,res){
+    dispatcher.onPost('/repoAudit', function (req, res) {
         var repo = new Repo();
-        repo.executeRest(req,res,'audit');
+        repo.executeRest(req, res, 'audit');
     })
-    var server = http.createServer((request, response) => {
+    var server = http.createServer((request, response) = > {
             try {
-                response.respond = function(status,msg,err)
-                {
-                    var respText = {};
-                    respText.msg = msg;
-                    if (err && err.hasOwnProperty('message')) {
-                        respText.error = err.message;
-                    }
-                    this.writeHead(status, {'Content-Type': 'application/json'});
-                    this.end(JSON.stringify(respText));
-                }; //response.respond
-                dispatcher.dispatch(request, response);
+                response.respond = function (status, msg, err) {
+                var respText = {};
+                respText.msg = msg;
+                if (err && err.hasOwnProperty('message')) {
+                    respText.error = err.message;
                 }
-    catch (err)
-        {
-        if (err.message === 'SHUTDOWN')
-        {
+                this.writeHead(status, {'Content-Type': 'application/json'});
+                this.end(JSON.stringify(respText));
+            };
+    dispatcher.dispatch(request, response);
+}
+catch
+    (err)
+    {
+        if (err.message === 'SHUTDOWN') {
             throw err;
         }
-        response.respond(503, "Error dispatching HTTP request",err);
+        response.respond(503, "Error dispatching HTTP request", err);
     }
-});
+})
+    ;
 
     // Startup the server
-    server.listen(3000, () => {
+    server.listen(3000, () = > {
         // Callback when server is successfully listening
 
-    });
+    }
+)
+    ;
 
 
     // Cleanup after ourselves if we get nerve-pinched
     process.on('SIGTERM', function () {
-        server.close(() => {
+        server.close(() = > {
             self.shutdown();
-    });
+    })
+        ;
     });
 
     // Cleanup after ourselves if we get nerve-pinched
     process.on('SIGTERM', function () {
-        server.close(() => {
+        server.close(() = > {
             self.shutdown();
-    });
+    })
+        ;
     })
 
 };
--- repo-copy_cli-shim.js
+++ repo-copy_cli-shim.js
@@ -1,8 +1,10 @@
-#! /usr/bin/env node
+#!/usr/bin/env node;
+
 
 /**
  * Created by bryancross on 7/13/18.
  */
+
 const repoController = require('./lib/repoController');
 const cla = require('command-line-args');
 const clu = require('command-line-usage');
@@ -10,89 +12,87 @@ const clu = require('command-line-usage');
 
 const optionDefinitions = [
     {name: 'templateRepoURL', alias: 't', type: String}
-    ,{name: 'targetRepoName',alias:'n', type: String}
-    ,{name: 'targetRepoOwner',alias:'o',type: String}
-    ,{name: 'mode',alias:'m', type: String, defaultValue: 'get'}
-    ,{name: 'help',alias:'h',type:Boolean}
-    ,{name: 'compareRepoURL',alias:'c',type:String}
-]
+    , {name: 'targetRepoName', alias: 'n', type: String}
+    , {name: 'targetRepoOwner', alias: 'o', type: String}
+    , {name: 'mode', alias: 'm', type: String, defaultValue: 'get'}
+    , {name: 'help', alias: 'h', type: Boolean}
+    , {name: 'compareRepoURL', alias: 'c', type: String}
+];
 
 const clSections = [
     {
-        header: 'repo-copy'
-        ,content: 'A utility to retrieve configuration info for and/or copy a repository.'
+        header: 'repo-copy',
+        content: 'A utility to retrieve configuration info for and/or copy a repository.'
     },
     {
-        header: 'Options'
-        ,optionList: [
-        {
-            name: 'templateURL'
-            ,alias: 't'
-            ,typeLabel: '{underline url}'
-            ,description: 'URL for the repository to copy or retrieve configuration info for.'
-        },
-        {
-            name: 'compareRepoURL'
-            ,alias: 'c'
-            ,typeLabel: '{underline url}'
-            ,description: 'URL to compare templateRepoURL to'
-        },
-        {
-            name: 'mode'
-            ,alias: 'm'
-            ,description: 'get|create|audit. \n get - return configuration data \n create - use configuration of <templateURL> to create a new repository\naudit - Return differences in configuration between template and target repos'
-        },
-        {
-            name: 'targetRepoName'
-            ,alias: 'n'
-            ,description: 'Name of the repo to be created, or, in the case of <audit> mode, compared against'
-        },
-        {
-            name: 'targetRepoOwner'
-            ,alias:'o'
-            ,description: 'Owner of the repo to be created, or, in the case of <audit> mode, compared against'
-        },
-        {
-            name: 'help'
-            ,alias: 'h'
-            ,type: Boolean
-            ,description: 'Display this message'
-        }
-    ]
+        header: 'Options',
+        optionList: [
+            {
+                name: 'templateURL',
+                alias: 't',
+                typeLabel: '{underline url}',
+                description: 'URL for the repository to copy or retrieve configuration info for.'
+            },
+            {
+                name: 'compareRepoURL',
+                alias: 'c',
+                typeLabel: '{underline url}',
+                description: 'URL to compare templateRepoURL to'
+            },
+            {
+                name: 'mode',
+                alias: 'm',
+                description: 'get|create|audit. \n get - return configuration data \n create - use configuration of <templateURL> to create a new repository\naudit - Return differences in configuration between template and target repos'
+            },
+            {
+                name: 'targetRepoName',
+                alias: 'n',
+                description: 'Name of the repo to be created, or, in the case of <audit> mode, compared against'
+            },
+            {
+                name: 'targetRepoOwner',
+                alias: 'o',
+                description: 'Owner of the repo to be created, or, in the case of <audit> mode, compared against'
+            },
+            {
+                name: 'help',
+                alias: 'h',
+                type: Boolean,
+                description: 'Display this message'
+            }
+        ]
     },
     {
-         header: 'Examples'
-        ,content: [
-        {
-            desc: '1. Retrieve repository configuration information'
-            ,example:  '$ node repo-copy -t https://github.com/myorg/myrepo'
-        },
-        {
-            desc: '2. Create a new repository copying the configuration of a template repository'
-            ,example: '$ node repo-copy -t https://github.com/myorg/myrepo -n myNewRepo -o myOtherOrg'
-        },
-        {
-            desc: '3. Retrieve a list of configuration differences between 2 repositories'
-            ,example: '$ node repo-copy -t https://github.com/sourceOrg/sourceRepo -n myCompareRepo -o myCompareOrg'
-        }
-    ]
+        header: 'Examples',
+        content: [
+            {
+                desc: '1. Retrieve repository configuration information',
+                example: '$ node repo-copy -t https://github.com/myorg/myrepo'
+            },
+            {
+                desc: '2. Create a new repository copying the configuration of a template repository',
+                example: '$ node repo-copy -t https://github.com/myorg/myrepo -n myNewRepo -o myOtherOrg'
+            },
+            {
+                desc: '3. Retrieve a list of configuration differences between 2 repositories',
+                example: '$ node repo-copy -t https://github.com/sourceOrg/sourceRepo -n myCompareRepo -o myCompareOrg'
+            }
+        ]
     },
     {
         content: 'Project home: {underline https://github.com/bidnessforb/repo-copy}'
     }
-]
+];
 
 const args = cla(optionDefinitions);
 
-if(args.help)
-{
+if (args.help) {
     console.log(clu(clSections));
     process.exit(0);
 }
 var errs = validateArgs(args);
 
-if(errs.length > 0)
-{
+if (errs.length > 0) {
     console.log('errors: ' + errs);
     console.log(clu(clSections));
     process.exit(1);
@@ -101,70 +101,60 @@ if(errs.length > 0)
 execute();
 
 
-function validateArgs(args)
-{
-    const validModes = ['get','create','audit'];
+function validateArgs(args) {
+    const validModes = ['get', 'create', 'audit'];
     const urlRegExp = /https:\/\/github.com\/[a-zA-Z0-9-_]+\/[a-zA-Z0-9-_]+$/g;
-    var errs = []
-    if(!args.templateRepoURL)
-    {
-        errs.push("Invalid template URL");
+    var errs = [];
+    if (!args.templateRepoURL) {
+        errs.push('Invalid template URL');
     }
-    if(args.templateRepoURL && !urlRegExp.test(args.templateRepoURL))
-    {
-        errs.push("Invalid URL: Must be https://github.com/<owner>/<repo>");
+    if (args.templateRepoURL && !urlRegExp.test(args.templateRepoURL)) {
+        errs.push('Invalid URL: Must be https://github.com/<owner>/<repo>');
     }
-    if(validModes.indexOf(args.mode) < 0 )
-    {
-        errs.push("Invalid mode. valid values are get, create, audit");
+    if (validModes.indexOf(args.mode) < 0) {
+        errs.push('Invalid mode. valid values are get, create, audit');
     }
-    if(args.mode == 'create' && !(args.targetRepoName && args.targetRepoOwner))
-    {
-        errs.push("Missing target repo information.  Please specify targetRepoName and targetRepoOwner");
+    if (args.mode == 'create' && !(args.targetRepoName && args.targetRepoOwner)) {
+        errs.push('Missing target repo information.  Please specify targetRepoName and targetRepoOwner');
     }
 
     return errs;
 }
 
 
-
-async function execute()
-{
+async
+function execute() {
     var repoData;
     var repo = new repoController();
-    try
-    {
+    try {
         //build a fake request
         var fakeRequest = {};
         var fakebody;
-        if(args.mode == 'get')
-        {
-            fakebody = [{URL:args.templateRepoURL}];
+        if (args.mode == 'get') {
+            fakebody = [{URL: args.templateRepoURL}];
         }
-        else if(args.mode == 'audit')
-        {
-            fakebody = [{URL:args.templateRepoURL},{URL:args.compareRepoURL}]
+        else if (args.mode == 'audit') {
+            fakebody = [{URL: args.templateRepoURL}, {URL: args.compareRepoURL}];
         }
-        else if (args.mode == 'create')
-        {
-            fakebody = {templateRepoURL:args.templateRepoURL
-                            ,newRepoOwner:args.newRepoOwner
-                            ,newRepoName:args.newRepoName
-                            ,mode:args.mode
-                            ,tokens:[]
-            }
+        else if (args.mode == 'create') {
+            fakebody = {
+                templateRepoURL: args.templateRepoURL,
+                newRepoOwner: args.newRepoOwner,
+                newRepoName: args.newRepoName,
+                mode: args.mode,
+                tokens: []
+            };
         }
         fakeRequest.body = JSON.stringify(fakebody);
         fakeRequest.headers = {};
         fakeRequest.headers.authorization = process.env.GH_PAT;
-        repo.executeRest(fakeRequest,null,args.mode, function(data){
+        repo.executeRest(fakeRequest, null, args.mode, function (data) {
             console.log(JSON.stringify(data));
             process.exit(0);
         });
     }
-    catch(err)
-    {
-        console.log("Error: " + err.message);
+    catch (err) {
+        console.log('Error: ' + err.message);
         process.exit(1);
     }
 
