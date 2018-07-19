96f4ed0ff36e82704b06d29c09ab4ce2f01e4118
Bryan Cross <bryancross@github.com>
2018-07-19 13:14:56 -0500

    Imlpemented JSON schema to validate arguments, improved error handling

--- createRepoController.js
+++ createRepoController.js
@@ -3,6 +3,7 @@
 const Repo = require('./lib/RepoController.js');
 const HttpDispatcher = require('httpdispatcher');
 const http = require('http');
+const fs = require('fs');
 var PORT = 3000;
 try {
     if(process.argv[2] && typeof eval(process.argv[2]) === 'number' && process.argv[2] > 1024 && process.argv[2] < 65535)
@@ -22,15 +23,18 @@ function initHTTPServer() {
 
     dispatcher = new HttpDispatcher();
     dispatcher.onPost('/createRepo', function (req, res) {
-        let repo = new Repo();
+        let repo;
+        repo = new Repo();
         repo.executeRest(req, res, 'create');
     });
     dispatcher.onPost('/getRepoConfig', function (req, res) {
-        let repo = new Repo();
+        let repo;
+        repo = new Repo();
         repo.executeRest(req, res, 'get');
     });
     dispatcher.onPost('/repoAudit', function (req, res) {
-        let repo = new Repo();
+        let repo;
+        repo = new Repo();
         repo.executeRest(req, res, 'audit');
     })
     server = http.createServer((request, response) => {
@@ -75,3 +79,4 @@ function initHTTPServer() {
             });
     });
 };
+
